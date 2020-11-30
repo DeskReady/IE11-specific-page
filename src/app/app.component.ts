f9481423cfebfab06ee0ddf2e601d6dc6668f2fb
TechNerdsUzi <uzairkhatri57@gmail.com>
2020-11-30 18:16:35 +0500

    remove code from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ie11';
-  ie: boolean = false;
-  constructor(){
-    if (this.isIE()) {
-      this.ie = true;
-    } else {
-      this.ie = false;
-    }
-  }
-
-  isIE() {
-    const ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
-    const msie = ua.indexOf('MSIE '); // IE 10 or older
-    const trident = ua.indexOf('Trident/'); //IE 11
-
-    return (msie > 0 || trident > 0);
+  constructor() {
   }
 }
